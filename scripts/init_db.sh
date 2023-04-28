d27ff7af1d1d6e32b28bf710b046f4ae077a50c1
nordluma <maxnordlund.95@hotmail.com>
2023-04-28 16:20:15 +0300

    feat: add migration

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -3,6 +3,19 @@
 set -x
 set -eo pipefail
 
+if ! [ -x "$(command -v psql)" ]; then
+    echo >&2 "Error: psql is not installed."
+    exit 1
+fi
+
+if ! [ -x "$(command -v sqlx)" ]; then
+    echo >&2 "Error: sqlx is not installed."
+    echo >&2 "Use:"
+    echo >&2 "cargo install sqlx"
+    echo >&2 "to install it"
+    exit
+fi
+
 # Check if a custom user has beens set, otherwise default to "postgres"
 DB_USER="${POSTGRES_USER:=postgres}"
 # Check if a custom password has been set, otherwise default to "postgres"
@@ -22,4 +35,16 @@ docker run \
     -p "${DB_PORT}":5432 \
     -d postgres \
     postgres -N 1000
-    #  Increased max number of connections for testing purposes
+# Increased max number of connections for testing purposes
+
+# Ping postgres until it's ready to accept commands
+until PGPASSWORD="${DB_PASSWORD}" psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+  >&2 echo "Postgres is still unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up and running on port ${DB_PORT}!"
+
+DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
+export DATABASE_URL
+sqlx migrate add create_subscription_table 
