326ed023ada0df9655c75877cf160706abcc1f83
nordluma <maxnordlund.95@hotmail.com>
2023-04-28 16:32:15 +0300

    feat: create subscriptions table

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -27,6 +27,9 @@ DB_PORT="${POSTGRES_PORT:=5432}"
 # Check if a custom host has been set, otherwise default to "localhost"
 DB_HOST="${POSTGRES_HOST:=localhost}"
 
+# Skip Docker if a dockerized Postgres db is already running
+if [[ -z "${SKIP_DOCKER}" ]]
+then
 # Launch postgres using Docker
 docker run \
     -e POSTGRES_USER=${DB_USER} \
@@ -36,6 +39,7 @@ docker run \
     -d postgres \
     postgres -N 1000
 # Increased max number of connections for testing purposes
+fi
 
 # Ping postgres until it's ready to accept commands
 until PGPASSWORD="${DB_PASSWORD}" psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
@@ -47,4 +51,7 @@ done
 
 DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 export DATABASE_URL
-sqlx migrate add create_subscription_table 
+sqlx database create
+sqlx migrate run
+
+>&2 echo "Postgres has been migrated, ready to go!"
