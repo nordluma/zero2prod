828541ef75620950a1495742b7def78d3e5bf535
nordluma <maxnordlund.95@hotmail.com>
2023-05-01 21:48:07 +0300

    refactor: move tracing span into #[tracing:instrument] procedural macro

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,17 +10,16 @@ pub struct FormData {
     name: String,
 }
 
-pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let request_id = Uuid::new_v4();
-    let request_span = tracing::info_span!(
-        "Adding a new subscriber.",
-        %request_id,
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        request_id = %Uuid::new_v4(),
         subscriber_email = %form.email,
         subscriber_name = %form.name
-    );
-
-    let _request_span_guard = request_span.enter();
-
+    )
+)]
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     let query_span = tracing::info_span!("Saving new subscriber details in the database");
 
     match sqlx::query!(
