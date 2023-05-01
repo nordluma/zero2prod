7cc6fae44d8d517d2b85110c0f777da5855d62b7
nordluma <maxnordlund.95@hotmail.com>
2023-05-01 18:26:56 +0300

    feat: add logger

--- src/startup.rs
+++ src/startup.rs
@@ -1,16 +1,19 @@
 use crate::routes::{health_check, subscribe};
+use actix_web::middleware::Logger;
 use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     // Wrap connection in web::Data which is equivalent to Arc smart pointer
-    let connection = web::Data::new(db_pool);
+    let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            // Middlewares ar added by using the "wrap" method on "App"
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
-            .app_data(connection.clone())
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
