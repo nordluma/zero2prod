b26bbb625cbccc4c741ecf45622ccfd3db8a5800
nordluma <maxnordlund.95@hotmail.com>
2023-05-07 21:00:31 +0300

    feat: add tracing-actix-web to trace request ids

--- src/startup.rs
+++ src/startup.rs
@@ -1,8 +1,8 @@
 use crate::routes::{health_check, subscribe};
-use actix_web::middleware::Logger;
 use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     // Wrap connection in web::Data which is equivalent to Arc smart pointer
@@ -10,7 +10,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let server = HttpServer::new(move || {
         App::new()
             // Middlewares ar added by using the "wrap" method on "App"
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
