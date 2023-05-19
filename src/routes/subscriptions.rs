a035d12dd7fdcc229ed1f43b92438ab840b35b12
nordluma <maxnordlund.95@hotmail.com>
2023-05-20 00:32:11 +0300

    perf: improve error handling in name validation

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -21,11 +21,16 @@ pub struct FormData {
     )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let name = match SubscriberName::parse(form.0.name) {
+        Ok(name) => name,
+        // Return early with a 400 if the name is invalid
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
     // `web::Form` is a wrappwer around `FormData`
     // `form.0` gives us access to the underlying `FormData`
     let new_subscriber = NewSubscriber {
         email: form.0.email,
-        name: SubscriberName::parse(form.0.name).expect("Name validation failed."),
+        name,
     };
 
     match insert_subscriber(&pool, &new_subscriber).await {
