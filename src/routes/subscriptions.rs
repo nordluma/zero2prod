d7556a51c4ea6c5ab6ac395bb799baf244c4c346
nordluma <maxnordlund.95@hotmail.com>
2023-05-20 01:26:05 +0300

    feat: add email validation

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -4,7 +4,7 @@ use sqlx::PgPool;
 use unicode_segmentation::UnicodeSegmentation;
 use uuid::Uuid;
 
-use crate::domain::{NewSubscriber, SubscriberName};
+use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -12,26 +12,20 @@ pub struct FormData {
     name: String,
 }
 
-#[tracing::instrument(
-    name = "Adding a new subscriber",
-    skip(form, pool),
-    fields(
-        subscriber_email = %form.email,
-        subscriber_name = %form.name
-    )
-)]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     let name = match SubscriberName::parse(form.0.name) {
         Ok(name) => name,
         // Return early with a 400 if the name is invalid
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
+
+    let email = match SubscriberEmail::parse(form.0.email) {
+        Ok(email) => email,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
     // `web::Form` is a wrappwer around `FormData`
     // `form.0` gives us access to the underlying `FormData`
-    let new_subscriber = NewSubscriber {
-        email: form.0.email,
-        name,
-    };
+    let new_subscriber = NewSubscriber { email, name };
 
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
@@ -72,7 +66,7 @@ pub async fn insert_subscriber(
     sqlx::query!(
         r#"INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)"#,
         Uuid::new_v4(),
-        new_subscriber.email,
+        new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
         Utc::now()
     )
