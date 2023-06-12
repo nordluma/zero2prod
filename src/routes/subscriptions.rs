639f5f00977794227844ee9c6ff6cfadc24d05f5
nordluma <maxnordlund.95@hotmail.com>
2023-06-12 21:45:30 +0300

    refactor: move parsing of FormData to an own function
    The separation gives a clearer view of responsibilites of the functions.
    `parse_subscriber` handles the conversion from wire format (url-decoded
    data collected from a HTML form) to our domain model (`NewSubscriber`).
    
    `subscribe` handles the generation of the HTTP response to the incoming
    HTTP request.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -12,20 +12,20 @@ pub struct FormData {
     name: String,
 }
 
+pub fn parse_subscriber(form: FormData) -> Result<NewSubscriber, String> {
+    let name = SubscriberName::parse(form.name)?;
+    let email = SubscriberEmail::parse(form.email)?;
+    Ok(NewSubscriber { email, name })
+}
+
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let name = match SubscriberName::parse(form.0.name) {
-        Ok(name) => name,
+    // `web::Form` is a wrappwer around `FormData`
+    // `form.0` gives us access to the underlying `FormData`
+    let new_subscriber = match parse_subscriber(form.0) {
+        Ok(subscriber) => subscriber,
         // Return early with a 400 if the name is invalid
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
-
-    let email = match SubscriberEmail::parse(form.0.email) {
-        Ok(email) => email,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-    // `web::Form` is a wrappwer around `FormData`
-    // `form.0` gives us access to the underlying `FormData`
-    let new_subscriber = NewSubscriber { email, name };
 
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
