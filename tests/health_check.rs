f943574e722c50ffb9dff0afcecaaf9907a95d28
nordluma <maxnordlund.95@hotmail.com>
2023-06-13 00:21:58 +0300

    test: add unit test for send_email post request

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -52,7 +52,12 @@ async fn spawn_app() -> TestApp {
         .email_client
         .sender()
         .expect("Invalid sender email address.");
-    let email_client = EmailClient::new(configuration.email_client.base_url, sender_email);
+
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+    );
 
     // Pass the new client to `run`
     let server =
