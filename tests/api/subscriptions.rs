8afab622fb6976b148349337357a50d978b307dc
nordluma <maxnordlund.95@hotmail.com>
2023-08-07 23:34:01 +0300

    test(subscriptions): test for successfully sent email

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -1,3 +1,8 @@
+use wiremock::{
+    matchers::{method, path},
+    Mock, ResponseTemplate,
+};
+
 use crate::helpers::spawn_app;
 
 #[tokio::test]
@@ -61,4 +66,21 @@ async fn subscribe_returns_400_when_data_is_missing() {
             error_msg
         )
     }
+}
+
+#[tokio::test]
+async fn subscribe_sends_a_confirmation_email_for_valid_data() {
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.into()).await;
+
+    // Mock asserts on drop
 }
