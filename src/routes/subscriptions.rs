ab4bdcdd761316346e1a7c2f53f8c304fa3c12b0
nordluma <maxnordlund.95@hotmail.com>
2023-08-08 00:11:29 +0300

    feat(subscriptions): send confirmation link

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -50,14 +50,23 @@ pub async fn subscribe(
         return HttpResponse::InternalServerError().finish();
     }
 
+    let confirmation_link = "https://there-is-no-such-domain.com/subscriptions/confirm";
+
     // Send a (useless) email to the new subscriber.
     // We are ignoring the email delivery errors for now.
     if email_client
         .send_email(
             new_subscriber.email,
             "Welcome!",
-            "Welcome to our newsletter",
-            "Welcome to our newsletter",
+            &format!(
+                "Welcome to our newsletter!<br />\
+                     Click <a href=\"{}\">here</a> to confirm your subscription.",
+                confirmation_link
+            ),
+            &format!(
+                "Welcome to our newsletter!\nVisit {} to confirm your subscription",
+                confirmation_link
+            ),
         )
         .await
         .is_err()
