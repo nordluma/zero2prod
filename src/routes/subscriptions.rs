102b1b39366708ec5049bfd8ed298e946ae18b65
nordluma <maxnordlund.95@hotmail.com>
2023-09-02 00:46:33 +0300

    feat(subscriptions): print formatted error chain for debugging

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -12,10 +12,30 @@ use crate::{
 };
 
 // A new error type, wrapping a `sqlx::Erro`
-#[derive(Debug)]
 pub struct StoreTokenError(sqlx::Error);
 
 impl ResponseError for StoreTokenError {}
+
+impl std::fmt::Debug for StoreTokenError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+fn error_chain_fmt(
+    e: &impl std::error::Error,
+    f: &mut std::fmt::Formatter<'_>,
+) -> std::fmt::Result {
+    writeln!(f, "{}\n", e)?;
+    let mut current = e.source();
+
+    while let Some(cause) = current {
+        writeln!(f, "Caused by:\n\t{}", cause)?;
+        current = cause.source();
+    }
+
+    Ok(())
+}
 
 impl std::fmt::Display for StoreTokenError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
