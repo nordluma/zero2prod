05e7e429013b88d255547f8ad8ecf7130a9450a5
nordluma <maxnordlund.95@hotmail.com>
2023-09-25 23:48:09 +0300

    feat(newsletters): query for all confirmed subscribers

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,5 +1,6 @@
 use actix_web::{web, HttpResponse};
 use serde::Deserialize;
+use sqlx::PgPool;
 
 #[derive(Deserialize)]
 pub struct BodyData {
@@ -19,4 +20,22 @@ struct ConfirmedSubscriber {
 
 pub async fn publish_newsletter(_body: web::Json<BodyData>) -> HttpResponse {
     HttpResponse::Ok().finish()
+}
+
+#[tracing::instrument(name = "Get confirmed subscribers", skip(pool))]
+async fn get_confirmed_subscribers(
+    pool: &PgPool,
+) -> Result<Vec<ConfirmedSubscriber>, anyhow::Error> {
+    let rows = sqlx::query_as!(
+        ConfirmedSubscriber,
+        r#"
+        SELECT email
+        FROM subscriptions
+        WHERE status = 'confirmed'
+        "#,
+    )
+    .fetch_all(pool)
+    .await?;
+
+    Ok(rows)
 }
