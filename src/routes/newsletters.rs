12b5320319883a276e4ca9aadf4fed62450ca794
nordluma <maxnordlund.95@hotmail.com>
2023-09-25 23:47:29 +0300

    feat(newsletters): define struct for request body

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,5 +1,22 @@
-use actix_web::HttpResponse;
+use actix_web::{web, HttpResponse};
+use serde::Deserialize;
 
-pub async fn publish_newsletter() -> HttpResponse {
+#[derive(Deserialize)]
+pub struct BodyData {
+    title: String,
+    content: Content,
+}
+
+#[derive(Deserialize)]
+pub struct Content {
+    html: String,
+    text: String,
+}
+
+struct ConfirmedSubscriber {
+    email: String,
+}
+
+pub async fn publish_newsletter(_body: web::Json<BodyData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
