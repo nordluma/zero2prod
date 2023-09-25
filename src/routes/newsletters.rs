c7674be706a30eb214af27e624e2c9a58aee0300
nordluma <maxnordlund.95@hotmail.com>
2023-09-25 23:55:26 +0300

    feat(newsletters): add enum for errors

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,6 +1,28 @@
-use actix_web::{web, HttpResponse};
+use actix_web::{http::StatusCode, web, HttpResponse, ResponseError};
 use serde::Deserialize;
 use sqlx::PgPool;
+
+use crate::routes::error_chain_fmt;
+
+#[derive(thiserror::Error)]
+pub enum PublishError {
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl std::fmt::Debug for PublishError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for PublishError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
 
 #[derive(Deserialize)]
 pub struct BodyData {
