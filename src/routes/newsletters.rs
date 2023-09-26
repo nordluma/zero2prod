002ce3119b0d9a4b166ba775b56bd54d1792f33a
nordluma <maxnordlund.95@hotmail.com>
2023-09-26 21:30:06 +0300

    feat(newsletters): decode basic auth header

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -6,6 +6,7 @@ use actix_web::{
     web, HttpRequest, HttpResponse, ResponseError,
 };
 use anyhow::Context;
+use base64::Engine;
 use secrecy::Secret;
 use serde::Deserialize;
 use sqlx::PgPool;
@@ -95,7 +96,39 @@ pub async fn publish_newsletter(
 }
 
 fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::Error> {
-    todo!()
+    // The header value, if present, must be a valid UTF-8 string
+    let header_value = headers
+        .get("Authorization")
+        .context("The 'Authorization' header was missing")?
+        .to_str()
+        .context("The 'Authorization' header was not a valid UTF-8 string.")?;
+
+    let base64encoded_segment = header_value
+        .strip_prefix("Basic ")
+        .context("The authorization scheme was not 'Basic'.")?;
+
+    let decoded_bytes = base64::engine::general_purpose::STANDARD
+        .decode(base64encoded_segment)
+        .context("Failed to base64-decode 'Basic' credentials.")?;
+
+    let decoded_credentials = String::from_utf8(decoded_bytes)
+        .context("The decoded credential string is not valid UTF-8.")?;
+
+    // Split into two segments, using ';' as delimiter
+    let mut credentials = decoded_credentials.splitn(2, ";");
+    let username = credentials
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("A username must be provided in 'Basic' auth."))?
+        .to_string();
+    let password = credentials
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("A password mut be provided in 'Basic' auth."))?
+        .to_string();
+
+    Ok(Credentials {
+        username,
+        password: Secret::new(password),
+    })
 }
 
 #[tracing::instrument(name = "Get confirmed subscribers", skip(pool))]
