de69de3787b83d2f3114f3fb757a2a43c399f5bb
nordluma <maxnordlund.95@hotmail.com>
2023-09-27 22:45:19 +0300

    style(newsletters): clippy linting

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -9,6 +9,7 @@ use anyhow::Context;
 use base64::Engine;
 use secrecy::{ExposeSecret, Secret};
 use serde::Deserialize;
+use sha3::Digest;
 use sqlx::PgPool;
 
 use crate::{domain::SubscriberEmail, email_client::EmailClient, routes::error_chain_fmt};
@@ -143,7 +144,7 @@ fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::Erro
         .context("The decoded credential string is not valid UTF-8.")?;
 
     // Split into two segments, using ':' as delimiter
-    let mut credentials = decoded_credentials.splitn(2, ":");
+    let mut credentials = decoded_credentials.splitn(2, ':');
     let username = credentials
         .next()
         .ok_or_else(|| anyhow::anyhow!("A username must be provided in 'Basic' auth."))?
@@ -163,14 +164,18 @@ async fn validate_credentials(
     credentials: Credentials,
     pool: &PgPool,
 ) -> Result<uuid::Uuid, PublishError> {
+    let password_hash = sha3::Sha3_256::digest(credentials.password.expose_secret().as_bytes());
+    // Lowercase hexadecimal encoding
+    let password_hash = format!("{:x}", password_hash);
+
     let user_id: Option<_> = sqlx::query!(
         r#"
         SELECT user_id
         FROM users
-        WHERE username = $1 AND password = $2
+        WHERE username = $1 AND password_hash = $2
         "#,
         credentials.username,
-        credentials.password.expose_secret(),
+        password_hash,
     )
     .fetch_optional(pool)
     .await
