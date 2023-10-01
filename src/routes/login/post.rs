ccaeccafc13af05be047c5cab74abd112cdbf592
nordluma <maxnordlund.95@hotmail.com>
2023-10-01 23:55:49 +0300

    feat(login): add error handling for login

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,5 +1,37 @@
-use actix_web::{http::header::LOCATION, web, HttpResponse};
+use actix_web::{
+    http::{header::LOCATION, StatusCode},
+    web, HttpResponse, ResponseError,
+};
 use secrecy::Secret;
+use sqlx::PgPool;
+
+use crate::{
+    authentication::{validate_credentials, AuthError, Credentials},
+    routes::error_chain_fmt,
+};
+
+#[derive(thiserror::Error)]
+pub enum LoginError {
+    #[error("Authentication failed")]
+    AuthError(#[source] anyhow::Error),
+    #[error("Something went wrong")]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl std::fmt::Debug for LoginError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for LoginError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            LoginError::AuthError(_) => StatusCode::UNAUTHORIZED,
+            LoginError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
