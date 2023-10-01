bd007bb908c302da4656ce0b4ce6e6ca154d0f07
nordluma <maxnordlund.95@hotmail.com>
2023-10-01 23:31:49 +0300

    feat(login): redirect to home page after submition

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,6 +1,13 @@
-use actix_web::{http::header::LOCATION, HttpResponse};
+use actix_web::{http::header::LOCATION, web, HttpResponse};
+use secrecy::Secret;
 
-pub async fn login() -> HttpResponse {
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    username: String,
+    password: Secret<String>,
+}
+
+pub async fn login(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::SeeOther()
         .insert_header((LOCATION, "/"))
         .finish()
