5922fa1c4deb13d6e0f0c8c85b4d2aef6d4368c0
nordluma <maxnordlund.95@hotmail.com>
2023-10-02 22:21:06 +0300

    test(helpers): assert if request has been redirected

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -218,3 +218,9 @@ async fn configure_database(config: &DataBaseSettings) -> PgPool {
 
     connection_pool
 }
+
+/// A helper function to assert if the request is redirected
+pub fn assert_is_redirect_to(response: &reqwest::Response, location: &str) {
+    assert_eq!(response.status().as_u16(), 303);
+    assert_eq!(response.headers().get("LOCATION").unwrap(), location);
+}
