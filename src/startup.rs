c5d6a4b48aacf4e1524671c0d8fa531d06469711
nordluma <maxnordlund.95@hotmail.com>
2023-10-03 22:03:55 +0300

    feat(startup): inject redis session store to app state

--- src/startup.rs
+++ src/startup.rs
@@ -1,3 +1,4 @@
+use actix_session::{storage::RedisSessionStore, SessionMiddleware};
 use actix_web::web::Data;
 use actix_web::{cookie::Key, dev::Server, web, App, HttpServer};
 use actix_web_flash_messages::{storage::CookieMessageStore, FlashMessagesFramework};
@@ -21,7 +22,7 @@ pub struct Application {
 impl Application {
     // We have converted the `build` function into a constructor for
     // `Application`
-    pub async fn build(configuration: Settings) -> Result<Self, std::io::Error> {
+    pub async fn build(configuration: Settings) -> Result<Self, anyhow::Error> {
         let connection_pool = get_connection_pool(&configuration.database);
 
         // Build an `EmailClient` using `configuration`
@@ -51,7 +52,9 @@ impl Application {
             email_client,
             configuration.application.base_url,
             configuration.application.hmac_secret,
-        )?;
+            configuration.redis_uri,
+        )
+        .await?;
 
         // "Save" the bound port in on of `Application`'s  fields
         Ok(Self { port, server })
@@ -81,26 +84,33 @@ pub struct ApplicationBaseUrl(pub String);
 #[derive(Clone)]
 pub struct HmacSecret(pub Secret<String>);
 
-pub fn run(
+pub async fn run(
     listener: TcpListener,
     db_pool: PgPool,
     email_client: EmailClient,
     base_url: String,
     hmac_secret: Secret<String>,
-) -> Result<Server, std::io::Error> {
+    redis_uri: Secret<String>,
+) -> Result<Server, anyhow::Error> {
     // Wrap connection in web::Data which is equivalent to Arc smart pointer
     let db_pool = web::Data::new(db_pool);
     let email_client = Data::new(email_client);
     let base_url = Data::new(ApplicationBaseUrl(base_url));
+    let secret_key = Key::from(hmac_secret.expose_secret().as_bytes());
 
     let message_store =
         CookieMessageStore::builder(Key::from(hmac_secret.expose_secret().as_bytes())).build();
     let message_framework = FlashMessagesFramework::builder(message_store).build();
+    let redis_store = RedisSessionStore::new(redis_uri.expose_secret()).await?;
 
     let server = HttpServer::new(move || {
         App::new()
             // Middlewares are added by using the "wrap" method on "App"
             .wrap(message_framework.clone())
+            .wrap(SessionMiddleware::new(
+                redis_store.clone(),
+                secret_key.clone(),
+            ))
             .wrap(TracingLogger::default())
             .route("/", web::get().to(home))
             .route("/health_check", web::get().to(health_check))
