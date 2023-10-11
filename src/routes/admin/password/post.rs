a76867074efca7ad5ee4303631fbb00173c142dc
nordluma <maxnordlund.95@hotmail.com>
2023-10-11 22:14:58 +0300

    feat(password): show error message as a flash message

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -1,4 +1,5 @@
 use actix_web::{web, HttpResponse};
+use actix_web_flash_messages::FlashMessage;
 use secrecy::{ExposeSecret, Secret};
 
 use crate::{
@@ -22,7 +23,13 @@ pub async fn change_password(
     }
 
     if form.new_password.expose_secret() != form.new_password_check.expose_secret() {
+        FlashMessage::error(
+            "You entered two different new passwords - the field values must match.",
+        )
+        .send();
+
         return Ok(see_other("/admin/password"));
     }
+
     todo!()
 }
