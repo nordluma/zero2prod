314c5c7345f09615ae18946eb3680c7adda3d42e
nordluma <maxnordlund.95@hotmail.com>
2023-10-11 21:46:56 +0300

    feat(utils): utility function for redirections

--- src/utils.rs
+++ src/utils.rs
@@ -7,3 +7,9 @@ where
 {
     actix_web::error::ErrorInternalServerError(e)
 }
+
+pub fn see_other(location: &str) -> HttpResponse {
+    HttpResponse::SeeOther()
+        .insert_header((LOCATION, location))
+        .finish()
+}
