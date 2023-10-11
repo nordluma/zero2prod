46856d28bdbf2cdf338dc3d7494085324c372fe2
nordluma <maxnordlund.95@hotmail.com>
2023-10-11 22:26:57 +0300

    feat(password): retrieve username

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -1,8 +1,10 @@
 use actix_web::{web, HttpResponse};
 use actix_web_flash_messages::FlashMessage;
 use secrecy::{ExposeSecret, Secret};
+use sqlx::PgPool;
 
 use crate::{
+    routes::admin::dashboard::get_username,
     session_state::TypedSession,
     utils::{e500, see_other},
 };
@@ -17,10 +19,15 @@ pub struct FormData {
 pub async fn change_password(
     form: web::Form<FormData>,
     session: TypedSession,
+    pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, actix_web::Error> {
-    if session.get_user_id().map_err(e500)?.is_none() {
+    let user_id = session.get_user_id().map_err(e500)?;
+
+    if user_id.is_none() {
         return Ok(see_other("/login"));
     }
+
+    let user_id = user_id.unwrap();
 
     if form.new_password.expose_secret() != form.new_password_check.expose_secret() {
         FlashMessage::error(
@@ -30,6 +37,8 @@ pub async fn change_password(
 
         return Ok(see_other("/admin/password"));
     }
+
+    let username = get_username(user_id, &pool).await.map_err(e500)?;
 
     todo!()
 }
