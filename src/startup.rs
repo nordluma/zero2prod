7f233dd7609bfd2a4ae51c38fa7c8ccf9cab97a1
nordluma <maxnordlund.95@hotmail.com>
2023-10-12 22:35:19 +0300

    feat(newsletter): form to publish newsletter issues

--- src/startup.rs
+++ src/startup.rs
@@ -14,7 +14,7 @@ use crate::{
     email_client::EmailClient,
     routes::{
         admin_dashboard, change_password, change_password_form, confirm, health_check, home,
-        log_out, login, login_form, publish_newsletter, subscribe,
+        log_out, login, login_form, publish_newsletter, publish_newsletter_form, subscribe,
     },
 };
 
@@ -128,6 +128,7 @@ pub async fn run(
                 web::scope("/admin")
                     .wrap(from_fn(reject_anonymous_users))
                     .route("/dashboard", web::get().to(admin_dashboard))
+                    .route("/newsletters", web::get().to(publish_newsletter_form))
                     .route("/password", web::get().to(change_password_form))
                     .route("/password", web::post().to(change_password))
                     .route("/logout", web::post().to(log_out)),
