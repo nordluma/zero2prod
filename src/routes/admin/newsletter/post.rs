f260675f44108cb37647502fd42ab7106cf6844d
nordluma <maxnordlund.95@hotmail.com>
2023-10-16 21:12:39 +0300

    refactor(newsletter): parse `idempotency_key``

--- src/routes/admin/newsletter/post.rs
+++ src/routes/admin/newsletter/post.rs
@@ -10,7 +10,8 @@ use crate::{
     authentication::UserId,
     domain::SubscriberEmail,
     email_client::EmailClient,
-    utils::{e500, see_other},
+    idempotency::IdempotencyKey,
+    utils::{e400, e500, see_other},
 };
 
 #[derive(serde::Deserialize)]
@@ -18,6 +19,7 @@ pub struct FormData {
     title: String,
     text_content: String,
     html_content: String,
+    idempotency_key: String,
 }
 
 #[tracing::instrument(
@@ -31,17 +33,22 @@ pub async fn publish_newsletter(
     pool: web::Data<PgPool>,
     email_client: web::Data<EmailClient>,
 ) -> Result<HttpResponse, actix_web::Error> {
+    // Destructure the form table to get around the borrow checker
+    let FormData {
+        title,
+        text_content,
+        html_content,
+        idempotency_key,
+    } = form.0;
+
+    let _idempotency_key: IdempotencyKey = idempotency_key.try_into().map_err(e400)?;
+
     let subscribers = get_confirmed_subscribers(&pool).await.map_err(e500)?;
     for subscriber in subscribers {
         match subscriber {
             Ok(subscriber) => {
                 email_client
-                    .send_email(
-                        &subscriber.email,
-                        &form.title,
-                        &form.text_content,
-                        &form.html_content,
-                    )
+                    .send_email(&subscriber.email, &title, &text_content, &html_content)
                     .await
                     .with_context(|| {
                         format!("Failed to send newsletter issue to {}", subscriber.email)
