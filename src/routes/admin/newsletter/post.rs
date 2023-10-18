e8be2b1734a359bbca6c867a2d6edb2898c4cfe7
nordluma <maxnordlund.95@hotmail.com>
2023-10-18 23:30:20 +0300

    refactor(newsletter): enqueue task into queue instead when publishing
    We'll enqueue the newsletter issue into the delivery queue and let
    background workers handle the delivery instead of iterating through all
    subscribers and sending as part of the request handling.

--- src/routes/admin/newsletter/post.rs
+++ src/routes/admin/newsletter/post.rs
@@ -9,8 +9,6 @@ use uuid::Uuid;
 
 use crate::{
     authentication::UserId,
-    domain::SubscriberEmail,
-    email_client::EmailClient,
     idempotency::{save_response, try_processing, IdempotencyKey, NextAction},
     utils::{e400, e500, see_other},
 };
@@ -25,13 +23,12 @@ pub struct FormData {
 
 #[tracing::instrument(
     name="Publish a newsletter issue", 
-    skip(form, pool, email_client, user_id),
+    skip_all,
     fields(user_id=%*user_id)
 )]
 pub async fn publish_newsletter(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
-    email_client: web::Data<EmailClient>,
     user_id: ReqData<UserId>,
 ) -> Result<HttpResponse, actix_web::Error> {
     let user_id = user_id.into_inner();
@@ -45,7 +42,7 @@ pub async fn publish_newsletter(
 
     let idempotency_key: IdempotencyKey = idempotency_key.try_into().map_err(e400)?;
 
-    let transaction = match try_processing(&pool, &idempotency_key, *user_id)
+    let mut transaction = match try_processing(&pool, &idempotency_key, *user_id)
         .await
         .map_err(e500)?
     {
@@ -57,68 +54,24 @@ pub async fn publish_newsletter(
         }
     };
 
-    let subscribers = get_confirmed_subscribers(&pool).await.map_err(e500)?;
-    for subscriber in subscribers {
-        match subscriber {
-            Ok(subscriber) => {
-                email_client
-                    .send_email(&subscriber.email, &title, &text_content, &html_content)
-                    .await
-                    .with_context(|| {
-                        format!("Failed to send newsletter issue to {}", subscriber.email)
-                    })
-                    .map_err(e500)?;
-            }
-            Err(error) => {
-                tracing::warn!(
-                    // We record the error chain as a structured field
-                    // on the log record.
-                    error.cause_chain = ?error,
-                    error.message = %error,
-                    // Using `\` to split a long string literal over
-                    // two lines, without creating a `\n` character.
-                    "Skipping a confirmed subscriber. \
-                    Their stored contact details are invalid",
-                );
-            }
-        }
-    }
+    let issue_id = insert_newsletter_issue(&mut transaction, &title, &text_content, &html_content)
+        .await
+        .context("Failed to store newsletter issue details")
+        .map_err(e500)?;
 
-    FlashMessage::info("The newsletter issue has been published!").send();
+    enqueue_delivery_tasks(&mut transaction, issue_id)
+        .await
+        .context("Failed to enqueue delivery tasks")
+        .map_err(e500)?;
 
     let response = see_other("/admin/newsletters");
     let response = save_response(transaction, &idempotency_key, *user_id, response)
         .await
         .map_err(e500)?;
 
+    success_message().send();
+
     Ok(response)
-}
-
-struct ConfirmedSubscriber {
-    email: SubscriberEmail,
-}
-
-#[tracing::instrument(name = "Get confirmed subscribers", skip(pool))]
-async fn get_confirmed_subscribers(
-    pool: &PgPool,
-) -> Result<Vec<Result<ConfirmedSubscriber, anyhow::Error>>, anyhow::Error> {
-    let confirmed_subscribers = sqlx::query!(
-        r#"
-        SELECT email
-        FROM subscriptions
-        WHERE status = 'confirmed'
-        "#
-    )
-    .fetch_all(pool)
-    .await?
-    .into_iter()
-    .map(|r| match SubscriberEmail::parse(r.email) {
-        Ok(email) => Ok(ConfirmedSubscriber { email }),
-        Err(e) => Err(anyhow::anyhow!(e)),
-    })
-    .collect();
-
-    Ok(confirmed_subscribers)
 }
 
 #[tracing::instrument(skip_all)]
@@ -175,5 +128,8 @@ async fn enqueue_delivery_tasks(
 }
 
 fn success_message() -> FlashMessage {
-    FlashMessage::info("The newsletter issue has been published!")
+    FlashMessage::info(
+        "The newsletter issue has been accepted - \
+                       emails will go out shortly",
+    )
 }
