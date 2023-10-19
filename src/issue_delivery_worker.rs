9082b86ee7fc9a1163e0f5cff9ca5cab6b3f3e3a
nordluma <maxnordlund.95@hotmail.com>
2023-10-19 17:56:35 +0300

    refactor: create `EmailClient` on demand

--- src/issue_delivery_worker.rs
+++ src/issue_delivery_worker.rs
@@ -24,19 +24,7 @@ struct NewsletterIssue {
 
 pub async fn run_worker_until_stopped(configuration: Settings) -> Result<(), anyhow::Error> {
     let connection_pool = get_connection_pool(&configuration.database);
-
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
-
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
+    let email_client = configuration.email_client.client();
 
     worker_loop(connection_pool, email_client).await
 }
